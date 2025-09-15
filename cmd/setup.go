32744efa8882f986aca892db6d216b19a1f251bb
van muscari <vamuscari@gmail.com>
2025-09-15 16:52:02 -0400

    Update gui flags

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,1 +1,24 @@
-package cmd
+var rootCmd = &cobra.Command{
+	Use:   "badgermaps",
+	Short: "A CLI for interacting with the BadgerMaps API",
+	Run: func(cmd *cobra.Command, args []string) {
+		// If no command is specified, and GUI is available, launch GUI
+		if hasGUI {
+			runGUI()
+		} else {
+			cmd.Help()
+		}
+	},
+}
+
+var guiCmd = &cobra.Command{
+	Use:   "gui",
+	Short: "Launches the graphical user interface",
+	Run: func(cmd *cobra.Command, args []string) {
+		if hasGUI {
+			runGUI()
+		} else {
+			fmt.Println("GUI not enabled in this build.")
+		}
+	},
+}
--- gui_disabled.go
+++ gui_disabled.go
@@ -2,11 +2,8 @@
 
 package main
 
-import "fmt"
-
 const hasGUI = false
 
 func runGUI() {
-	fmt.Println("This version of the application was built without GUI support.")
-	fmt.Println("To build with the GUI, use the '-tags gui' flag.")
+	// No GUI support
 }
